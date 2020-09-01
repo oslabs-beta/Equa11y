099c9cba7a9996d2de2276f36ba2efd01cda9a93
Tjolanda Sullivan <sully@tjolandas-mini.myfiosgateway.com>
2020-09-01 14:16:48 -0400

    building initial shell

---
+++ index.ts
@@ -0,0 +1,22 @@
+const chalk = require('chalk');
+const clear = require('clear');
+const figlet = require('figlet');
+const inquirerFile  = require('./lib/inquirer.ts');
+
+clear();
+
+const run = async () => {
+  const credentials = await inquirerFile.askPreferences();
+  // console.log(credentials);
+
+  // chalk.keyword(credentials)
+  
+    console.log(
+      chalk.keyword(credentials.color)(
+        figlet.textSync(credentials.phrase, { horizontalLayout: 'full' })
+      )
+    )
+  
+};
+
+run();
