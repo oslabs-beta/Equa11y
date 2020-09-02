68342fb3566caffba073a1bf7f8a9dde0eff1460
Taylor Riley Du <taylorsriley@gmail.com>
2020-09-02 17:00:22 -0400

    Changed require to import + added types

--- index.ts
+++ index.ts
@@ -1,26 +1,21 @@
 #!/usr/bin/env node
 
-const chalk = require('chalk');
-const clear = require('clear');
-const figlet = require('figlet');
-const inquirerFile  = require('./lib/inquirer.ts');
+import chalk from 'chalk';
+import clear from 'clear';
+import figlet from 'figlet';
+import { inquirerFile } from './src/inquirer';
 
 clear();
 
 const run = async () => {
   const credentials = await inquirerFile.askPreferences();
-  // console.log(credentials);
-
-  // chalk.keyword(credentials)
-  
-    console.log(
-      chalk.keyword(credentials.color)(
-        figlet.textSync(credentials.phrase, { horizontalLayout: 'full' })
-      )
-    )
-  
+  console.log(
+    chalk.keyword(credentials.color)(
+      figlet.textSync(credentials.phrase, { horizontalLayout: 'full' }),
+    ),
+  );
 };
 
 run();
 
-module.exports = run;
+// module.exports = run;
