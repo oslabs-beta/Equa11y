dc43fc2f033fc8319e52039b41261a59c799f8dc
Heather Friedman <hfried25@gmail.com>
2020-09-05 10:27:26 -0400

    error handling

--- src/index.ts
+++ src/index.ts
@@ -14,14 +14,22 @@ const spinner = new Spinner('Loading, please wait!');
 
 console.log(chalk.cyan(figlet.textSync('Equa11y', { horizontalLayout: 'full' })));
 const run = async () => {
-  const inputURL = await inquirerFile.askPreferences();
-  spinner.start();
-  const data = await puppet(inputURL.url);
-  spinner.stop();
+  try {
+    const inputURL = await inquirerFile.askPreferences();
+    spinner.start();
+    const data = await puppet(inputURL.url);
 
-  const loop = await inquirerFile.askLoop(data);
-  if (loop.res === 'quit') process.exit(0);
-  else if (loop.res === 'search again') run();
+    spinner.stop();
+
+    const loop = await inquirerFile.askLoop(data);
+    if (loop.res === 'quit') process.exit(0);
+    else if (loop.res === 'search again') run();
+  } catch (error) {
+    spinner.stop();
+    const errors = await inquirerFile.askError();
+    if (errors.startOver === 'quit') process.exit(0);
+    else if (errors.startOver === 'search again') run();
+  }
 };
 
 run();
