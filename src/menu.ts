e892e2826c8658bd2ef781d6c2406289b2dcfb92
Heather Friedman <hfried25@gmail.com>
2020-09-07 11:27:09 -0400

    updated typescript

--- src/menu.ts
+++ src/menu.ts
@@ -1,23 +1,29 @@
+interface MenuContents {
+  levelName: string;
+  opened: boolean;
+  arrows: string[];
+  subLevel: any[];
+}
+
 interface Dropdown {
-  create(levelName: string, subLevel:any[], opened?: boolean): object;
+  create(levelName: string, subLevel: any[], opened?: boolean): MenuContents;
   stringify(levelObj: any): string;
 }
 
 export const menu: Dropdown = {
-
   create: (levelName, subLevel, opened = false) => {
     return {
-      levelName: levelName,
-      opened: opened,
+      levelName,
+      opened,
       arrows: ['⇒', '⇓', '⇨', '⇩'],
-      subLevel: subLevel,
+      subLevel,
     };
   },
 
-  stringify: (levelObj) => {
+  stringify: levelObj => {
     let option = '';
     option += levelObj.opened ? levelObj.arrows[1] : levelObj.arrows[0];
     option += ` ${levelObj.levelName} (${levelObj.subLevel.length}) issues`;
     return option;
-  }
+  },
 };
--- src/prompts.ts
+++ src/prompts.ts
@@ -1,6 +1,6 @@
 import inquirer from 'inquirer';
-import { menu } from './menu';
 import { Result } from 'axe-core';
+import { menu } from './menu';
 
 interface Prompts {
   askPath(): Promise<{ url: string }>;
@@ -23,11 +23,17 @@ export const prompts: Prompts = {
   askLoop: results => {
     // sort issues into common occurances
     const sorted = results.reduce((acc: any, cur: any) => {
-      (acc[cur.impact]) ? acc[cur.impact].push(cur) : acc[cur.impact] = [ cur ];
+      if (acc[cur.impact]) {
+        acc[cur.impact].push(cur);
+      } else {
+        acc[cur.impact] = [cur];
+      }
       return acc;
     }, {});
     // process sorted issues into menu form
-    const processed = Object.keys(sorted).map((issueLevel: any) => menu.create(issueLevel, sorted[issueLevel]));
+    const processed = Object.keys(sorted).map((issueLevel: any) =>
+      menu.create(issueLevel, sorted[issueLevel]),
+    );
     // compile an array of stringified menu options
     const options = processed.map((option: any) => menu.stringify(option));
     const questions = [
