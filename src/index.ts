2189a727388d92a25bf5fc6a5b317d31ec2f4366
Will Hack <will.j.hack@gmail.com>
2020-09-08 10:22:37 -0400

    Menu top level opening and closing. Second level showing errors but missing titles

--- src/index.ts
+++ src/index.ts
@@ -4,34 +4,62 @@ import chalk from 'chalk';
 import clear from 'clear';
 import figlet from 'figlet';
 import CLI from 'clui';
+import { Result } from 'axe-core';
 import { prompts } from './prompts';
 import { puppet } from './puppeteer';
 
-clear();
+interface Program {
+  start(): Promise<void>;
+  loop(data: Result[], path: string, targetLevel?: string): Promise<void>;
+}
 
 const { Spinner } = CLI;
 const spinner = new Spinner('Loading, please wait!');
 
-console.log(chalk.cyan(figlet.textSync('Equa11y', { horizontalLayout: 'full' })));
-const run = async () => {
-  try {
-    const inputURL = await prompts.askPath();
-    spinner.start();
-    const data = await puppet(inputURL.url); // real prompt for publishing
-    // const data = await puppet('http://www.google.com'); // optional hardcoding for dev
-    spinner.stop();
+export const program: Program = {
+  start: async () => {
+    // Heading creation
+    clear();
+    console.log(chalk.cyan(figlet.textSync('Equa11y', { horizontalLayout: 'full' })));
+    // Ask for URL/localpath
+    try {
+      const inputURL = { url: 'http://www.google.com' }; // optional hardcoding for dev
+      // const inputURL = await prompts.askPath(); // real prompt for publishing
+      spinner.start();
+      const data = await puppet(inputURL.url);
+      spinner.stop();
+      // Ask user for next step
+      await program.loop(data, inputURL.url);
+    } catch (error) {
+      spinner.stop();
+      const errors = await prompts.askError();
+      if (errors.startOver === 'quit') process.exit(0);
+      else if (errors.startOver === 'search again') program.start();
+    }
+  },
 
-    const loop = await prompts.askLoop(data);
-    if (loop.res === 'quit') process.exit(0);
-    else if (loop.res === 'search again') run();
-  } catch (error) {
-    spinner.stop();
-    const errors = await prompts.askError();
-    if (errors.startOver === 'quit') process.exit(0);
-    else if (errors.startOver === 'search again') run();
-  }
-};
+  loop: async (results, path, targetLevel) => {
+    // Reset the display
+    clear();
+    console.log(chalk.cyan(figlet.textSync('Equa11y', { horizontalLayout: 'full' })));
+    console.log(chalk.bold('Input URL:'), path);
 
-run();
+    const options = await prompts.askOptions(results, targetLevel);
+    if (options.res === 'quit') process.exit(0);
+    else if (options.res === 'search again') program.start();
+    // Parse leading arrow
+    else {
+      const arrow = options.res.trim()[0];
+      if (arrow === '⇒') {
+        const targetLevel = options.res.trim().split(' ')[1];
+        // console.log(targetLevel);
+        program.loop(results, path, targetLevel);
+      } else {
+        program.loop(results, path);
+      }
+    }
+  },
 
-// module.exports = run;
+}
+
+program.start();
--- src/menu.ts
+++ src/menu.ts
@@ -1,23 +1,59 @@
+import { Result } from 'axe-core';
+
+export interface MenuContents {
+  levelName: string,
+  opened: boolean,
+  nested: boolean,
+  arrows: string[],
+  subLevel: any[],
+}
+
 interface Dropdown {
-  create(levelName: string, subLevel:any[], opened?: boolean): object;
-  stringify(levelObj: any): string;
+  askMenu(results: Result[], level?:string): string[];
+  processLevel(levelName: string, subLevel:[], opened?: boolean, nested?: boolean): MenuContents;
+  stringify(levelObj: any, nested: boolean): string;
 }
 
 export const menu: Dropdown = {
 
-  create: (levelName, subLevel, opened = false) => {
+  askMenu: (results, targetLevel) => {
+    // sort issues into common occurances i.e. { critical: [resultItem1, resultItem2], severe: [resultItem3]}
+    const sorted = results.reduce((acc: any, cur: any) => {
+      if (acc[cur.impact]) acc[cur.impact].push(cur);
+      else acc[cur.impact] = [cur];
+      return acc;
+    }, {});
+    // process sorted issues into menu form
+    const processed:any = [];
+    Object.keys(sorted).forEach((issueLevel: string) => {
+      if (issueLevel === targetLevel) {
+        processed.push(menu.processLevel(issueLevel, sorted[issueLevel], true));
+        sorted[issueLevel].forEach((issue: any) => {
+          processed.push(menu.processLevel(issue, issue.nodes, false, true));
+        });
+      }
+      else processed.push(menu.processLevel(issueLevel, sorted[issueLevel]));
+    });
+    // compile an array of stringified menu options
+    const options = processed.map((option: MenuContents) => menu.stringify(option, option.nested));
+    return (options);
+  },
+
+  processLevel: (levelName, subLevel, opened = false, nested = false) => {
     return {
-      levelName: levelName,
-      opened: opened,
+      levelName,
+      opened,
+      nested,
       arrows: ['⇒', '⇓', '⇨', '⇩'],
-      subLevel: subLevel,
+      subLevel,
     };
   },
 
-  stringify: (levelObj) => {
+  stringify: (levelObj, nested) => {
     let option = '';
+    if (nested) option += '  ';
     option += levelObj.opened ? levelObj.arrows[1] : levelObj.arrows[0];
-    option += ` ${levelObj.levelName} (${levelObj.subLevel.length}) issues`;
+    option += ` ${levelObj.levelName} (${levelObj.subLevel.length}) issues types: TBD total sub issues`;
     return option;
   }
 };
