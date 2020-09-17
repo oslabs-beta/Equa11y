a647a1669ab864d3726d742cd93a389031ce3d1d
Heather Friedman <hfried25@gmail.com>
2020-09-17 17:11:26 -0400

    deleted server

--- src/index.ts
+++ src/index.ts
@@ -25,8 +25,8 @@ export const program: Program = {
     console.log(chalk.cyan(figlet.textSync('Equa11y', { horizontalLayout: 'full' })));
     // Ask for URL/localpath
     try {
-      const inputURL = { url: 'http://google.com' }; // optional hardcoding for dev
-      // const inputURL = await prompts.askPath(); // real prompt for publishing
+      //  const inputURL = { url: 'http://google.com' }; // optional hardcoding for dev
+      const inputURL = await prompts.askPath(); // real prompt for publishing
       spinner.start();
       const data = await puppet(inputURL.url);
       const parsed = dataParser(data);
